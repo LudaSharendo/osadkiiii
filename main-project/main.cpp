284b398e7e7cb36ce930f0d913747306857cabb5
LudaSharendo <154054387+LudaSharendo@users.noreply.github.com>
2023-12-21 09:40:22 +0300

    Добавлен вывод общей информации о задании

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -1,7 +1,11 @@
 #include <iostream>
 
+using namespace std;
+
 int main()
 {
-    std::cout << "Hello\n";
+    cout << "Laboratory work #9. GIT\n";
+    cout << "Variant #3. осадки\n";
+    cout << "Author: Шарендо Людмила\n";
     return 0;
 }

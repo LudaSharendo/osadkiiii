da1cdb02f932335258ad245465459c9ad27e8fe2
LudaSharendo <154054387+LudaSharendo@users.noreply.github.com>
2023-12-22 19:03:25 +0300

    Добавлены тестовые данные

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -7,5 +7,6 @@ int main()
     cout << "Laboratory work #9. GIT\n";
     cout << "Variant #3. osadki\n";
     cout << "Author: Sharendo Liudmila\n";
+    cout << "Group: 23_Ping\n";
     return 0;
 }
